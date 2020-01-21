c6ebfc2d5a9bb4981ef024ab6b3ed9a995ab9e8f
Jan Bliznicenko <jan.bliznicenko@fit.cvut.cz>
2020-01-21 22:46:45 +0100

    enabled deployment

--- scripts/ssh.sh
+++
@@ -1,4 +0,0 @@
-openssl aes-256-cbc -K $encrypted_d01ef4b1b1ec_key -iv $encrypted_d01ef4b1b1ec_iv -in .ssh/id_borm_travis.enc -out .ssh/id_borm_travis -d
-chmod 0400 .ssh/id_borm_travis
-eval $(ssh-agent -s)
-ssh-add .ssh/id_borm_travis
